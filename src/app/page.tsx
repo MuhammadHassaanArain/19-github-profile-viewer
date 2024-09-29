aa8de47629a4c2010f8d1750654f4ee4b923c623
Hassaan Arain <rafatnaz21@gmail.com>
2024-09-29 18:32:55 +0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+import GithubProfileViewerComponent from '../components/github-profile'
+export default function Home() {
+  return (
+   <div>
+    <GithubProfileViewerComponent />
+   </div>
+  );
+}
